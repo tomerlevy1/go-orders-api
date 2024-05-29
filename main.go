d66e08246aba7bb2de6a551eb1d6d90af46d8c4a
Tomer <tolevy@outbrain.com>
2024-05-29 22:28:07 +0300

    feat: create a simple http server

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	server := &http.Server{
+		Addr:    ":3100",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error starting server: ", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
 }
